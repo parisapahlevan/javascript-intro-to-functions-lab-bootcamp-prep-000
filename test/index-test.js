2b3519446936e7f5d3f5e24db964a410737d89b2
parisa pahlevan <paris.pahlevan@gmail.com>
2018-06-19 19:38:49 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,7 @@
 
-describe('shout(string)', function() {
-  it('receives one argument and returns it in all caps', function() {
+describe('shout(string)', function shout(string) {
+  return string.
+   {
     expect(shout('hello')).toEqual('HELLO')
   })
 })
